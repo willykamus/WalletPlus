38965b930fe8875dbbb2887ccd6c3e44c6414f82
William Ching <willykamus@gmail.com>
2021-04-19 19:11:56 -0400

    First Commit

---
+++ WalletPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalletPlus
+//
+//  Created by William Ching on 2021-04-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WalletPlus/WalletPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletPlusApp.swift
+//  WalletPlus
+//
+//  Created by William Ching on 2021-04-19.
+//
+
+import SwiftUI
+
+@main
+struct WalletPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
