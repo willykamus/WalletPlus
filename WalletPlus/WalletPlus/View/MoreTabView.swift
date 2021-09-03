e249329a4557e653510446c05983b6043927aa39
William Ching <willykamus@gmail.com>
2021-09-03 14:12:13 -0400

    Added more in tab bar

---
+++ WalletPlus/WalletPlus/View/MoreTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MoreTabView.swift
+//  WalletPlus
+//
+//  Created by William Ching on 2021-09-03.
+//
+
+import SwiftUI
+
+struct MoreTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoreTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreTabView()
+    }
+}
